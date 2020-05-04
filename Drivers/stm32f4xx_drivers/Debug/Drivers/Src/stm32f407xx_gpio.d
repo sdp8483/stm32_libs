10634dc537459fc108f78b4a14259b78aa9c358e
sdp8483 <17242713+sdp8483@users.noreply.github.com>
2020-05-04 19:39:25 -0400

    gitignore fix

--- Drivers/stm32f4xx_drivers/Debug/Drivers/Src/stm32f407xx_gpio.d
+++
@@ -1,7 +0,0 @@
-Drivers/Src/stm32f407xx_gpio.o: ../Drivers/Src/stm32f407xx_gpio.c \
- /home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h \
- /home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h
-
-/home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx_gpio.h:
-
-/home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
--- Drivers/stm32f4xx_drivers/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- /home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h
-
-/home/sam/STM32CubeIDE/MCU1/stm32f4xx_drivers/Drivers/Inc/stm32f407xx.h:
